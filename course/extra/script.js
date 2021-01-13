d239b4a102da44ae42eb7ad91d8ece96c5d1c6f9
Aung-Moe-Oo <coderaung@gmail.com>
2021-01-13 15:22:25 +0630

    First Commit

---
+++ course/extra/script.js
@@ -0,0 +1,54 @@
+window.sr = ScrollReveal();
+                sr.reveal('.nav-section',{
+                    origin:'top',
+                    duration: 500,
+                    distance: '200px',
+                });
+                sr.reveal('.designLeft',{
+                    origin:'left',
+                    duration: 1000,
+                    distance: '200px',
+                    delay: 400,
+                });
+                sr.reveal('.designRight',{
+                    origin:'right',
+                    duration: 1000,
+                    distance: '200px',
+                    delay: 400,
+                });
+                sr.reveal('.serviceLeft',{
+                    origin:'left',
+                    duration: 1000,
+                    distance: '200px',
+                    delay: 400,
+                });
+                sr.reveal('.serviceRight',{
+                    origin:'bottom',
+                    duration: 1000,
+                    distance: '200px',
+                    delay: 400,
+                });
+                sr.reveal('.devLeft',{
+                    origin:'left',
+                    duration: 1000,
+                    distance: '200px',
+                    delay: 400,
+                });
+                sr.reveal('.devCenter',{
+                    origin:'bottom',
+                    duration: 1000,
+                    distance: '200px',
+                    delay: 400,
+                });
+                sr.reveal('.devRight',{
+                    origin:'right',
+                    duration: 1000,
+                    distance: '200px',
+                    delay: 400,
+                });
+                sr.reveal('.smallHeader',{
+                    origin:'bottom',
+                    duration: 2000,
+                    distance: '200px',
+                    delay: 400,
+                });
